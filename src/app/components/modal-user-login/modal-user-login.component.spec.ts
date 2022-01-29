388dd68a13d27ac3fcd3d3b0263245d7fcc5cd99
Emilio <egb1982@gmail.com>
2022-01-29 10:44:14 +0100

    first update

--- src/app/components/modal-user-login/modal-user-login.component.spec.ts
+++ src/app/components/modal-user-login/modal-user-login.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ModalUserLoginComponent } from './modal-user-login.component';
 
@@ -6,7 +6,7 @@ describe('ModalUserLoginComponent', () => {
   let component: ModalUserLoginComponent;
   let fixture: ComponentFixture<ModalUserLoginComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ModalUserLoginComponent ]
     })
--- src/app/components/recipe-form/recipe-form.component.spec.ts
+++ src/app/components/recipe-form/recipe-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RecipeFormComponent } from './recipe-form.component';
 
@@ -6,7 +6,7 @@ describe('RecipeFormComponent', () => {
   let component: RecipeFormComponent;
   let fixture: ComponentFixture<RecipeFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RecipeFormComponent ]
     })
